a5dcfd642f043fcd5a32b9ac6e973af25e4f6d8b
Rickyfre <113684777+Rickyfre@users.noreply.github.com>
2025-07-30 14:25:09 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var tablinks = document.getElementsByClassName("tab__links");
 var tabcontents = document.getElementsByClassName("tab__contents");
 
 function opentab(tabname){
+
     for(tablink of tablinks){
         tablink.classList.remove("active__link");
     }
@@ -17,3 +18,18 @@ function opentab(tabname){
 
     document.getElementById(tabname).classList.add("active__tab");
 }
+
+const navMenu = document.querySelector('.nav__menu');
+const openBtn = document.getElementById('open-menu-btn');
+const closeBtn = document.getElementById('close-menu-btn');
+
+openMenuBtn.addEventListener('click', () => {
+    navMenu.classList.add('active');
+})
+
+closeBtn.addEventListener('click', () => {
+    navMenu.classList.remove('active');
+})
+
+
+
