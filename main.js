764716e8c9132383f369b5043f5f011956381a2f
Rickyfre <113684777+Rickyfre@users.noreply.github.com>
2025-08-01 16:17:20 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -19,17 +19,26 @@ function opentab(tabname){
     document.getElementById(tabname).classList.add("active__tab");
 }
 
-const navMenu = document.querySelector('.nav__menu');
-const openBtn = document.getElementById('open-menu-btn');
-const closeBtn = document.getElementById('close-menu-btn');
 
-openMenuBtn.addEventListener('click', () => {
-    navMenu.classList.add('active');
+// show/hide nav menu
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
 })
 
-closeBtn.addEventListener('click', () => {
-    navMenu.classList.remove('active');
-})
+
+function closeNav() {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
 
 
 
